32568217a70ad33f7cf745a6210e8f1f0f09a417
Myitschoolby <info@myitschool.by>
2020-11-09 21:28:01 +0300

    Complete slider products

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -5,6 +5,41 @@ import '../css/catalog.css';
 class Catalog extends React.Component {
     constructor() {
         super();
+
+        this.slideNextPrev = this.slideNextPrev.bind(this);
+    }
+
+    slideNextPrev(event) {
+        const btn = event.target == 'SPAN' ? event.target.closest('button') : event.target;
+        const dir = !btn.dataset.dir ? 'next' : btn.dataset.dir;
+
+        const products_container = document.querySelectorAll('.catalog_products_list')[0];
+        const products_container_width = products_container.offsetWidth;
+
+        const products_list = products_container.querySelectorAll('li');
+
+        const product_first = products_list[0];
+        const product_first_styles = window.getComputedStyle(product_first);
+
+        let product_first_width = product_first.offsetWidth;
+        let product_first_ml = product_first_styles.getPropertyValue('margin-left');
+        let product_first_mr = product_first_styles.getPropertyValue('margin-right');
+
+        if (product_first_mr) {
+            product_first_mr = parseInt(product_first_mr);
+            product_first_width = product_first_width + product_first_mr;
+        }
+
+        if (product_first_ml) {
+            product_first_ml = Math.abs(parseInt(product_first_ml));
+        }
+
+        let ml = 0;
+        const stopPoint = (products_list.length * product_first_width) - (Math.ceil(products_container_width / product_first_width) * product_first_width);
+
+        if (product_first_ml < (stopPoint + product_first_width)) ml = product_first_ml + product_first_width * (dir == 'prev' ? -1 : 1);
+
+        product_first.style.marginLeft = `-${ml}px`;
     }
 
     render() {
@@ -20,8 +55,8 @@ class Catalog extends React.Component {
                         </div>
                     </div>
                     <div className="catalog_nav">
-                        <button className="catalog_nav_btn_prev"><span className="ico_arrow"></span></button>
-                        <button className="catalog_nav_btn_next"><span className="ico_arrow"></span></button>
+                        <button data-dir="prev" onClick={this.slideNextPrev} className="catalog_nav_btn_prev"><span className="ico_arrow"></span></button>
+                        <button data-dir="next" onClick={this.slideNextPrev} className="catalog_nav_btn_next"><span className="ico_arrow"></span></button>
                     </div>
                 </div>
                 <div className="catalog_products">
@@ -31,7 +66,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 1</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -44,7 +79,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 2</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -57,7 +92,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 3</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -70,7 +105,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 4</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -83,7 +118,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 5</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -96,7 +131,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 6</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -109,7 +144,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 7</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -122,7 +157,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 8</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
@@ -135,7 +170,7 @@ class Catalog extends React.Component {
                                 <img src="https://via.placeholder.com/150x200" />
                             </div>
                             <div className="product_caption">
-                                <div className="product_name">Face Mask</div>
+                                <div className="product_name">Face Mask 9</div>
                                 <div className="product_description">Grape Fruit</div>
                                 <div className="product_price">
                                     <div className="product_price_value">$15.50</div>
